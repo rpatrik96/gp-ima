50fd6646179cdb68aeacd6d76e25f619038f69d7
Patrik Reizinger <prmedia17@gmail.com>
2023-04-14 13:33:03 +0200

    C_ima_digamma calculation to jupyter

--- gp_ima/ima.py
+++ gp_ima/ima.py
@@ -3,11 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.special import digamma
 
 
-def C_ima(d, D):
+def C_ima_digamma(d, D):
     return d * digamma(D / 2) - sum(
         [digamma(D / 2 + (1 - i) / 2) for i in range(1, d + 1)]
     )
-
-
-Ds = np.logspace(0, 3, 1000).astype(int)
-plt.plot(Ds, [C_ima(max(1, int(np.log(D))), D) for D in Ds])
