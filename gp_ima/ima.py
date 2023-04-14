1f3b1a752ad17992ecfd5d5f6ff9602ecb2fad62
Patrik Reizinger <prmedia17@gmail.com>
2023-04-14 13:35:23 +0200

    C_ima_sample

--- gp_ima/ima.py
+++ gp_ima/ima.py
@@ -1,9 +1,21 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.special import digamma
 
 
 def C_ima_digamma(d, D):
     return d * digamma(D / 2) - sum(
         [digamma(D / 2 + (1 - i) / 2) for i in range(1, d + 1)]
+    )
+
+
+def C_ima_sample(gplvm):
+    z = np.random.normal(gplvm.X.mean, gplvm.X.variance**0.5)
+    dmu_dX, dv_dX = gplvm.predict_jacobian(z)  # this seems to be latents * data dim
+    dv_dX[dv_dX <= 0] = 1e-6
+    jacobian_sample = np.random.normal(dmu_dX, dv_dX**0.5)
+    jacobian_sample = np.einsum("aik,ajk->aij", jacobian_sample, jacobian_sample).mean(
+        axis=0
+    )
+    return np.log(np.linalg.det(np.diag(np.diag(jacobian_sample)))) - np.log(
+        np.linalg.det(jacobian_sample)
     )
