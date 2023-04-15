721192f3fc7fbe42590d041fa2d6edbd4d2a4ff5
Patrik Reizinger <prmedia17@gmail.com>
2023-04-15 13:58:14 +0200

    test_C_ima_sample

--- tests/test_ima.py
+++ tests/test_ima.py
@@ -1,2 +1,15 @@
+from gp_ima.ima import C_ima_sample
+import GPy
+import numpy as np
+
+
 def test_dummy():
     pass
+
+
+def test_C_ima_sample():
+    X = np.random.randn(10, 2)
+    kernel = GPy.kern.RBF(2, ARD=False) + GPy.kern.Bias(2)
+    m = GPy.models.GPLVM(np.asarray(X), 2, kernel=kernel)
+
+    C_ima_sample(m)
