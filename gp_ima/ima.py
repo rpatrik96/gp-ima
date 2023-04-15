9099498fa9ac6eb2490f8b672bb96963f234bda5
Patrik Reizinger <prmedia17@gmail.com>
2023-04-15 13:57:44 +0200

    cima function

--- gp_ima/ima.py
+++ gp_ima/ima.py
@@ -1,6 +1,6 @@
+import GPy
 import numpy as np
 from scipy.special import digamma
-import GPy
 
 
 def C_ima_digamma(d, D):
@@ -17,9 +17,24 @@ def C_ima_sample(gplvm):
     dmu_dX, dv_dX = gplvm.predict_jacobian(z)  # this seems to be latents * data dim
     dv_dX[dv_dX <= 0] = 1e-6
     jacobian_sample = np.random.normal(dmu_dX, dv_dX**0.5)
-    jacobian_sample = np.einsum("aik,ajk->aij", jacobian_sample, jacobian_sample).mean(
-        axis=0
-    )
-    return np.log(np.linalg.det(np.diag(np.diag(jacobian_sample)))) - np.log(
-        np.linalg.det(jacobian_sample)
+    jacobian_sample = np.einsum("aik,ajk->aij", jacobian_sample, jacobian_sample)
+    jacobian_mean = np.einsum("aik,ajk->aij", dmu_dX, dmu_dX)
+    # .mean(axis=0)
+
+    return np.mean([cima_kl_diagonality(j) for j in jacobian_mean])
+
+    # return np.log(np.linalg.det(np.diag(np.diag(jacobian_sample)))) - np.log(  #     np.linalg.det(jacobian_sample)  # )
+
+
+def cima_kl_diagonality(jacobian_t_jacobian: np.ndarray):
+    """
+    Calculates the IMA constrast. Able to handle jax and Pytorch objects as well
+
+    :param jacobian: jacobian matrix transpose time jacobian matrix
+    :return:
+    """
+
+    return 0.5 * (
+        np.linalg.slogdet(np.diag(np.diag(jacobian_t_jacobian)))[1]
+        - np.linalg.slogdet(jacobian_t_jacobian)[1]
     )
