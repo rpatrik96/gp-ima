5ac44e68f18c6368e9d8c6f2f2331ced683f8da6
Patrik Reizinger <prmedia17@gmail.com>
2023-07-14 15:52:48 +0200

    0.5 in c_ima_digamma + notebook plots

--- gp_ima/ima.py
+++ gp_ima/ima.py
@@ -4,8 +4,9 @@ from scipy.special import digamma
 
 
 def C_ima_digamma(d, D):
-    return d * digamma(D / 2) - sum(
-        [digamma(D / 2 + (1 - i) / 2) for i in range(1, d + 1)]
+    return 0.5 * (
+        d * digamma(D / 2)
+        - sum([digamma(D / 2 + (1 - i) / 2) for i in range(1, d + 1)])
     )
 
 
@@ -16,14 +17,9 @@ def C_ima_sample(gplvm):
         z = np.random.normal(gplvm.X.mean, gplvm.X.variance**0.5)
     dmu_dX, dv_dX = gplvm.predict_jacobian(z)  # this seems to be latents * data dim
     dv_dX[dv_dX <= 0] = 1e-6
-    jacobian_sample = np.random.normal(dmu_dX, dv_dX**0.5)
-    jacobian_sample = np.einsum("aik,ajk->aij", jacobian_sample, jacobian_sample)
     jacobian_mean = np.einsum("aik,ajk->aij", dmu_dX, dmu_dX)
-    # .mean(axis=0)
 
     return np.mean([cima_kl_diagonality(j) for j in jacobian_mean])
-
-    # return np.log(np.linalg.det(np.diag(np.diag(jacobian_sample)))) - np.log(  #     np.linalg.det(jacobian_sample)  # )
 
 
 def cima_kl_diagonality(jacobian_t_jacobian: np.ndarray):
